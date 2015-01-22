c1cfc152f171dbb2cc487b769571750556b13bd2
Devyn <devyn.challman@gmail.com>
2015-01-22 14:08:29 -0800

    bug fix

--- spaurk/ground0/views.py
+++ spaurk/ground0/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from ground0.models import Artist
+
 # Create your views here.
+def index(request):
+    latest_artist_list = Artist.objects.order_by('-dateTime')[:5]
+    context = {'latest_artist_list': latest_artist_list}
+
+    return render(request, 'ground0/index.html', context)
