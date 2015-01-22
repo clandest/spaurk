ecb545624fdb5d3e8c60a32fdc0e13514905dc98
Devyn <devyn.challman@gmail.com>
2015-01-21 21:10:52 -0800

    added Artist and Song models

--- spaurk/ground0/models.py
+++ spaurk/ground0/models.py
@@ -6,4 +6,24 @@ class Post(models.Model):
     pub_date = models.DateTimeField()
     text = models.TextField()
 
+class Artist(models.Model):
+    name = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    twitter = models.CharField(max_length=50)
+    facebook = models.CharField(max_length=50)
+    youtube = models.CharField(max_length=50)
+    website = models.CharField(max_length=50)
+    dateTime = models.DateTimeField()
 
+    def __str__(self)
+        return self.name
+
+class Song(models.Model):
+    artist = models.ForeignKey(Artist)
+    title = models.CharField(max_length=30)
+    sample = models.CharField(max_length=50)
+    spaurkCount = models.IntegerField(default=0)
+    dateTime = models.DateTimeField()
+
+    def __str__(self)
+        return self.title
